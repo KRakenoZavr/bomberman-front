9abc5d8b732a8d56e39ba62662047a09fc1063b5
KRakenoZavr <kuzembay@gmail.com>
2024-09-12 14:52:43 +0500

    fix: map -> GameMap

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,35 +1,28 @@
 import { Websocket, WebsocketEvent, WebsocketEventMap } from "websocket-ts";
-import { ENV, getEnv } from "../cfg/cfg";
-import { IMap, Map } from "./map";
+import { ENV, getEnv } from "../cfg/env";
+import { GameMap, IMap } from "./map";
 import { WS } from "./ws";
-import { WsMapEventData } from "./ws/events";
 
 export class Game {
   ws: WS;
   env: ENV;
-  map: Map;
+  map: GameMap;
 
   constructor(map: IMap) {
     this.env = getEnv();
     this.ws = new WS(this.env.WS_URL, this.env.BACKOFF_TIMER);
-    this.map = new Map(map);
+    this.map = new GameMap(map);
 
     const event = (
       i: Websocket,
       ev: WebsocketEventMap[WebsocketEvent.message]
     ) => {
-      this.map.handler.handle_event(
-        this.map.getMap(),
-        i,
-        ev.data as WsMapEventData
-      );
-      this.render()
+      this.map.handle(i, ev.data);
+      this.render();
     };
 
     this.ws.addMessageListener(event);
   }
 
-  render() {
-    
-  }
+  render() {}
 }
